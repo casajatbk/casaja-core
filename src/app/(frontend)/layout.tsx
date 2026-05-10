65d2c12e6149bca119abc25383f9abf2cf4d76cf
Excel <excelvalen.ev@gmail.com>
2026-05-10 22:26:25 +0700

    feat: Add title web

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
+import type { Metadata } from 'next'
 
 import './globals.css'
+
 import Navbar from '@/client/components/navbar'
 import Footer from '@/client/components/footer'
 import DeviceInit from '@/client/components/DeviceInit'
@@ -9,9 +11,35 @@ type LayoutProps = {
   children: ReactNode
 }
 
+const logoUrl = 'https://kaj5bd5xtsvgxojs.public.blob.vercel-storage.com/logo-casaja.jpeg'
+
+export const metadata: Metadata = {
+  title: 'CASAJA - Cable Rental Service for BINUS Students',
+  description: 'Layanan penyewaan kabel roll untuk mahasiswa BINUS',
+
+  icons: {
+    icon: logoUrl,
+    shortcut: logoUrl,
+    apple: logoUrl,
+  },
+
+  openGraph: {
+    title: 'CASAJA',
+    description: 'Layanan penyewaan kabel roll untuk mahasiswa BINUS',
+    images: [logoUrl],
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CASAJA',
+    description: 'Layanan penyewaan kabel roll untuk mahasiswa BINUS',
+    images: [logoUrl],
+  },
+}
+
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <html lang="en">
+    <html lang="id">
       <body>
         <DeviceInit />
 
